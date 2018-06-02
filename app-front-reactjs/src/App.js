adc66e3380751049bd0bc8cc9ea09e4777fc51c9
Abdeljalil NACEUR <contact@naceur-abdeljalil.com>
2018-06-02 13:24:37 +0200

    React_front init

---
+++ app-front-reactjs/src/App.js
@@ -0,0 +1,16 @@
+import  React, { Component } from "react";
+import { Redirect, Link, Route, Switch } from "react-router-dom";
+import { router, route, Browserhistory } from "react-router";
+
+class App extends Component {
+    render() {
+        return (
+            <div className="row">
+                <Switch>
+                </Switch>
+            </div>
+        );
+    }
+}
+
+export default App; 
