dcb1048078f965b351d845ab03804c1067e92a8e
ancap <mathiaspeterhorst@gmail.com>
2018-06-02 18:16:00 +0200

    tab navigation ok, normalement plus de bugs

--- Kether/app/src/main/java/com/bob/kether/Home.java
+++ Kether/app/src/main/java/com/bob/kether/Home.java
@@ -1,13 +1,15 @@
 package com.bob.kether;
 
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.TableLayout;
 
 public class Home extends AppCompatActivity {
 
@@ -16,16 +18,47 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-//        TabLayout tabLayout = (TabLayout) findViewById(R.id.home_tab);
-//        tableLayout.
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.home_tab);
+        tabLayout.addTab(tabLayout.newTab().setText("Buy"));
+        tabLayout.addTab(tabLayout.newTab().setText("Sell"));
+        tabLayout.addTab(tabLayout.newTab().setText("Generate original contract"));
+        //set tab gravity to fill the entire layout
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        // On utilise le PagerAdapter pour gérer les fragments à afficher
+        // Chaque page est représentée par son propre fragment
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.home_viewPager);
+        final PagerAdapter pagerAdapter = new PagerAdapter(
+                getSupportFragmentManager(),
+                tabLayout.getTabCount()
+        );
+        viewPager.setAdapter(pagerAdapter);
+
+        // Setting a Listener for clicks
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
     }
 
-    public class PageAdapter extends FragmentStatePagerAdapter {
-        public int mNumOfTabs;
+    // PagerAdapter permet d'afficher chaque fragment correspondant à la position dans les tabs
+    public class PagerAdapter extends FragmentStatePagerAdapter {
+        int mNumOfTabs;
 
-        public PageAdapter(FragmentManager fm, int numOfTabs) {
+        public PagerAdapter(FragmentManager fm, int NumOfTabs) {
             super(fm);
-            this.mNumOfTabs = numOfTabs;
+            this.mNumOfTabs = NumOfTabs;
         }
 
         @Override
@@ -47,8 +80,4 @@ public class Home extends AppCompatActivity {
             return mNumOfTabs;
         }
     }
-
-//    public void frag1_scanQRCode(View view) {
-//        Log.d("ok", "ok");
-//    }
 }
